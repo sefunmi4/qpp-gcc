e502369298b4aed727af774a36d758396f1b558c
Sefunmi Ashiru <sefunmiashiru0@gmail.com>
2025-09-07 23:12:35 -0400

    Add backend abstraction and stubs

---
+++ include/qpp/backend/Backend.hpp
@@ -0,0 +1,30 @@
+#ifndef QPP_BACKEND_BACKEND_HPP
+#define QPP_BACKEND_BACKEND_HPP
+
+#include <string>
+
+namespace qpp {
+
+/// Result of executing a circuit on a backend.
+struct RunResult {
+    bool success = true;
+};
+
+/// Abstract base class for all qpp backends.
+class Backend {
+public:
+    virtual ~Backend() = default;
+
+    /// Returns true if the backend is available for use.
+    virtual bool available() const = 0;
+
+    /// Load a circuit description into the backend.
+    virtual void loadCircuit(const std::string& circuit) = 0;
+
+    /// Execute the loaded circuit and return the results.
+    virtual RunResult run() = 0;
+};
+
+} // namespace qpp
+
+#endif // QPP_BACKEND_BACKEND_HPP
