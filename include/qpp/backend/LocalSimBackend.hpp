e15ee108db41ceae60bf7018decf95d06a1a3061
Sefunmi Ashiru <sefunmiashiru0@gmail.com>
2025-09-08 00:36:03 -0400

    feat: add noise modeling and square wave encoding

--- include/qpp/backend/LocalSimBackend.hpp
+++ include/qpp/backend/LocalSimBackend.hpp
@@ -15,7 +15,9 @@ class LocalSimBackend : public Backend {
 public:
     using StateVector = qclass;
 
-    LocalSimBackend(int max_qubits = 0, bool use_fftw = false, unsigned seed = 0);
+    LocalSimBackend(int max_qubits = 0, bool use_fftw = false, unsigned seed = 0,
+                    double phase_jitter = 0.0, double amplitude_noise = 0.0,
+                    double depolarizing = 0.0);
 
     bool available() const override { return true; }
     void loadCircuit(const std::string& circuit) override;
@@ -36,6 +38,13 @@ private:
     unsigned shots_ = 0;
     std::vector<Operation> ops_;
     std::vector<int> measuredQubits_;
+
+    // Noise parameters
+    double phaseJitter_ = 0.0;
+    double amplitudeNoise_ = 0.0;
+    double depolarizingProb_ = 0.0;
+
+    void applyNoise();
 };
 
 } // namespace qpp
